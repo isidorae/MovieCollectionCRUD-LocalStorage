59b6629d0f5f4faa078010d33a9686ccf9349ffa
isidorae <mariaisidoraerrazuriz@gmail.com>
2023-07-14 09:32:52 -0400

    CRUDAPP-AgregandoEditFunction

--- javascript.js
+++ javascript.js
@@ -140,8 +140,12 @@
 // // "<option>OBRA MAESTRA<div>💎</div></option>", comment:"Increible!",}
 // ]
 
- //HIDE UPADATE BUTTON
 
+
+
+
+
+ //HIDE UPADATE BUTTON
 function hideUpdateButton () {
     const updateButton = document.querySelector("#boton-actualizar") 
     updateButton.style.display = "none" //default --> display: inline-block;
@@ -192,6 +196,9 @@ function addDataLocalStorage(event) {
     if(messages.length > 0) {
         event.preventDefault();
         errorMsg.innerHTML = messages.join(' ');
+    } else {
+        //eliminar msjes de error
+        errorMsg.style.display = "none";
     }
 
     //conectar variable con key local storage
@@ -205,9 +212,13 @@ function addDataLocalStorage(event) {
     //******************** IDENTIFICADOR NEW MOVIE PARA DELETE
     //creamos key lastMovieId
     function getMovieID() {
+        //lastMovieId sera otro "key" de nuestro local storage, si el valor es null (no tiene nada dentro), lo igualamos a -1, asi nos aseguramos que exista key
         let lastMovieId = localStorage.getItem("lastMovieId") || "-1";
+        //parseamos lastMovieId (transformarlo a numero) y le sumamos 1, se podria usar parseInt tambien,, le agrega 1 a cada lastItem, entonces 0,1,2,3 etc
         let newMovieId = JSON.parse(lastMovieId) + 1;
+        //agregamos id nuevo al local storage
         localStorage.setItem("lastMovieId", JSON.stringify(newMovieId))
+        //retornamos valor de id valido que sera el valor encontado dentro del objeto
         return newMovieId;
     }
    
@@ -223,7 +234,7 @@ function addDataLocalStorage(event) {
    document.getElementById("anio").value = ""
    document.getElementById("specificSizeSelect").value = "Rating"
    document.getElementById("exampleFormControlTextarea1").value = ""
-
+  
 }
 
 
@@ -236,10 +247,10 @@ function showDataHtml() {
     let movieKey; 
     if(localStorage.getItem("movieKey") === null) {
         movieKey = [];
-        movieTable.style.display = "none" //esconder header 
+       // movieTable.style.display = "none" //esconder header 
     } else {
         movieKey = JSON.parse(localStorage.getItem("movieKey"))
-        movieTable.style.display = "table-row" //mostrar header
+      //  movieTable.style.display = "table-row" //mostrar header
        
     }
 
@@ -256,7 +267,7 @@ function showDataHtml() {
           <td>${element.rating}</td>
           <td><i>"${element.comment}"</i></td>
           <td>
-          <a href="#" onclick="editData(${index})" class="btn btn-warning btn-sm edit">Editar</a>
+          <a href="#" onclick="editData(event)" class="btn btn-warning btn-sm edit">Editar</a>
           <a href="#" onclick="remove(event)" class="btn btn-danger btn-sm delete">Eliminar</a>
           </td>
           </tr>`
@@ -264,19 +275,37 @@ function showDataHtml() {
   })
 
   cuerpoLista.innerHTML = htmlElements;
+  displayTableRow() 
 }
 
 document.onload = showDataHtml()
 
-//******************** DELETE DATA
+//************* HIDE AND SHOW TABLLE ROW
+
+    function displayTableRow() {
+         //tabla header
+        const movieTable = document.querySelector("tr") 
+   
+        let movieKey; 
+        if(localStorage.getItem("movieKey") === null || localStorage.getItem("movieKey") === "[]" ) {
+            movieTable.style.display = "none" //esconder header 
+        } else {
+            movieKey = JSON.parse(localStorage.getItem("movieKey"))
+            movieTable.style.display = "table-row" //mostrar header
+        
+        }
+    }
+
+//********************************** DELETE DATA
 
 //le paso como parametro ID de movie dentro de obj que quiero eliminar 
 function remove(event) {
+    event.preventDefault()
 
-    // event.target.parentNode.parentNode.remove();
-    let accederListaHTML = event.target.parentNode.parentNode; //agarra fila a eliminar
+    let accederListaHTML = event.target.parentNode.parentNode; 
     let movieRow = document.getElementById("movieContainer") 
     let accessID = movieRow.getAttribute("movieid"); 
+    console.log("accessDelete ID: " + accessID)
     accederListaHTML.remove()
     deleteMovieIdObj(accessID); //eliminar del local storage
  }
@@ -284,127 +313,211 @@ function remove(event) {
 
 
 
- //********guardar movie ID en local Storage
+ //Eliminar elemento con ID especifico del local Storage y actualizarlo 
 function deleteMovieIdObj(movieID) {
-    //obtengo
+    //obtengo movieKey [{..},..]
     let movieArrObj = JSON.parse(localStorage.getItem("movieKey")) //obtenemos array con objetos
-    //busco movie ID de pelicula que quiero eliminar
+    //busco movie ID de pelicula que quiero eliminar (indice de elemento a eliminar)
     let movieIndexInArray =  movieArrObj.findIndex(element => element.movieID === movieID)
-    //Elimino elemento unicamente 1 elemento
+    console.log("moveindexArray: " + movieIndexInArray)
+    //Elimino elemento, y unicamente 1 elemento
     movieArrObj.splice(movieIndexInArray, 1)
 
+    //ingresamos array editado al local storage, convertir a JSON y luego se guarda con setitem
     let movieArrayJSON = JSON.stringify(movieArrObj);
-
     localStorage.setItem("movieKey", movieArrayJSON);
 
 }
 
 
-
-// let arrmovies = JSON.parse(localStorage.getItem("movieKey"))
-// console.log(arrmovies[0].movieID) //asi accedemos a propiedad de objeto 
+//******************** EDIT DATA - CLICK EDIT
 
 
-// console.log(localStorage.getItem("movieKey".movie))
-//******************** EDIT DATA
+//editar inputs con valor
+        function editData(event) { 
+            event.preventDefault()
 
-    function editData(index) {
-
-        //cambiar botones 
-        const updateButton = document.querySelector("#boton-actualizar") 
-        updateButton.style.display = "inline-block" 
-
-        const submitButton = document.querySelector("#boton-form")
-        submitButton.style.display = "none" 
-
-          //obtener data del local storage
-        let movieKey; 
-         if(localStorage.getItem("movieKey") === null) {
-        movieKey = [];
-        } else {
-        movieKey = JSON.parse(localStorage.getItem("movieKey"))
+            let movieRow = document.getElementById("movieContainer") 
+            let accessID = movieRow.getAttribute("movieid"); 
+            console.log("access ID: " + accessID)
+        
+            //deleteMovieIdObj(accessID); //eliminar del local storage
+            accessDataFromId(accessID)
+    
+       
         }
 
-        //mostrar valores en submit form 
-       document.getElementById("movie").value = movieKey[index].movie
-       document.getElementById("anio").value = movieKey[index].anio
-       document.getElementById("specificSizeSelect").value = movieKey[index].rating
-       document.getElementById("exampleFormControlTextarea1").value = movieKey[index].comment
-    
-console.log(movieKey[index].movie)
-console.log(document.getElementById("movie").value)
+    console.log("*******EDITANDO")
 
+// acceder a ID de local storage
+        function accessDataFromId(movieID) {
 
-       document.getElementById("boton-actualizar").onclick = function () {
+            //cambiar botones 
+         const updateButton = document.querySelector("#boton-actualizar") 
+         updateButton.style.display = "inline-block" 
 
-        //validadores
+        const submitButton = document.querySelector("#boton-form")
+         submitButton.style.display = "none" 
 
-        //actualizar data en table, pero falta anclarla despues 
-            movieKey[index].movie = document.getElementById("movie").value
-            movieKey[index].anio = document.getElementById("anio").value
-            movieKey[index].rating = document.getElementById("specificSizeSelect").value
-            movieKey[index].comment = document.getElementById("exampleFormControlTextarea1").value
-
+            //obtenemos array con objetos
+            let movieKey = JSON.parse(localStorage.getItem("movieKey"))
             console.log(movieKey)
-            console.log(JSON.stringify(movieKey))
+            //busco movie ID de pelicula que quiero EDITAR
+        let movieIndexInArray =  movieKey.findIndex(element => {
+           if (element.movieID === movieID) 
+           //mostrar valores en submit form
+           return
+           document.getElementById("movie").value = element.movie
+           document.getElementById("anio").value = element.anio
+           document.getElementById("specificSizeSelect").value = element.rating
+           document.getElementById("exampleFormControlTextarea1").value = element.comment
 
-        
-        //anclar al local storage para que se actualize misma fila
+        })
+       
+    
+        }
+    
+//******************** EDIT DATA - UPDATE
+
+      function updateDataOnClick(event) {
+        event.preventDefault()
+
+        //Get movie ID of movie being edited
+        let movieRow = document.getElementById("movieContainer") 
+        let accessID = movieRow.getAttribute("movieid"); 
+        console.log("accessID: " + accessID)
+
+         //get movieKey from local storage
+         let movieKey = JSON.parse(localStorage.getItem("movieKey"))
+         console.log(movieKey)
+
+        //get the updated values from form fields
+        let updatedMovie = document.getElementById("movie").value 
+        let updatedAnio = document.getElementById("anio").value
+        let updatedRating = document.getElementById("specificSizeSelect").value 
+        let updatedComment = document.getElementById("exampleFormControlTextarea1").value
+
+        console.log("upadted movie: " + updatedMovie)
+    
+
+
+        //encontrar movie object que haga match con movie ID
+        let movieToUpdate = movieKey.find((movie) => {
+
+        movie.movieID === accessID
+            return movie
+        });
+
+        console.log(movieToUpdate)
+
+
+        //update valores de prop de objeto con nuevos valores
+        movieToUpdate.movie = updatedMovie;
+        movieToUpdate.anio = updatedAnio;
+        movieToUpdate.rating = updatedRating;
+        movieToUpdate.comment = updatedComment;
+
+       
+
+        //guardar movieKey array actualizado en local storage
         localStorage.setItem("movieKey", JSON.stringify(movieKey))
 
-        console.log(movieKey) //con esto cache que el problema es el local storage
+        //clear form fields
+        document.getElementById("movie").value = "";
+        document.getElementById("anio").value = "";
+        document.getElementById("specificSizeSelect").value = "";
+        document.getElementById("exampleFormControlTextarea1").value = "";
 
-        //actualizar en html 
-        showDataHtml()
-
-
-        //  document.getElementById("movie").value = ""
-        //  document.getElementById("anio").value = ""
-        //  document.getElementById("specificSizeSelect").value = ""
-        //  document.getElementById("exampleFormControlTextarea1").value = ""
-
-         //cambiar botones 
-         const updateButton = document.querySelector("#boton-actualizar") 
-         updateButton.style.display = "none" 
+          //cambiar botones 
+          const updateButton = document.querySelector("#boton-actualizar") 
+          updateButton.style.display = "none" 
  
          const submitButton = document.querySelector("#boton-form")
-         submitButton.style.display = "inline-block" 
+          submitButton.style.display = "inline-block" 
 
+          //mostrar data html
+          showDataHtml()
 
-       }
-
-
-
-    }
-
+        }
 
 
 
-//******************** DELETE DATA FROM LOCAL STORAGE
+    // function accessDataToUpdate(movieID) {
+    //     let movieKey = JSON.parse(localStorage.getItem("movieKey"))
+    //     console.log(movieKey)
+    //     //busco movie ID de pelicula que quiero EDITAR
+    // let movieIndexInArray =  movieKey.findIndex(element => {
+    //    if (element.movieID === movieID) 
+    //    //mostrar valores en submit form
+    //    return 
+    //    element.movie = document.getElementById("movie").value 
+    //    element.anio = document.getElementById("anio").value
+    //    element.rating = document.getElementById("specificSizeSelect").value 
+    //    element.comment = document.getElementById("exampleFormControlTextarea1").value
+    // })
+    // }
 
-//     function remove() {
 
-     
 
-//         const movieContainer = document.getElementById("movieContainer")
 
-//          //get data from local storage, parse it 
-//         let movieKey; 
-//             movieKey = JSON.parse(localStorage.getItem("movieKey"))
+//     function editData(index) {
 
-//             console.log(JSON.parse(localStorage.getItem("movieKey")).splice(index))
+//         index.preventDefault()
+//         //cambiar botones 
+//         const updateButton = document.querySelector("#boton-actualizar") 
+//         updateButton.style.display = "inline-block" 
 
-//         //let spliceMovieArray = movieKey.splice(index)
-//         //console.log(spliceMovieArray)
+//         const submitButton = document.querySelector("#boton-form")
+//         submitButton.style.display = "none" 
 
-//         //console.log(movieKey[index]) //Test para ver como acceder a cada obj
-//         // localStorage.removeItem("movieKey", index); //elimina del local storage
-//         //  movieContainer.remove() //eliminar de html
+//         let movieKey = JSON.parse(localStorage.getItem("movieKey"))
+
+//         //mostrar valores en submit form 
+//        document.getElementById("movie").value = movieKey[index].movie
+//        document.getElementById("anio").value = movieKey[index].anio
+//        document.getElementById("specificSizeSelect").value = movieKey[index].rating
+//        document.getElementById("exampleFormControlTextarea1").value = movieKey[index].comment
+    
+// console.log(movieKey[index].movie)
+// console.log(document.getElementById("movie").value)
+
+
+//        document.getElementById("boton-actualizar").onclick = function () {
+
+//         //validadores
+
+//         //actualizar data en table, pero falta anclarla despues 
+//             movieKey[index].movie = document.getElementById("movie").value
+//             movieKey[index].anio = document.getElementById("anio").value
+//             movieKey[index].rating = document.getElementById("specificSizeSelect").value
+//             movieKey[index].comment = document.getElementById("exampleFormControlTextarea1").value
+
+//             console.log(movieKey)
+//             console.log(JSON.stringify(movieKey))
+
+        
+//         //anclar al local storage para que se actualize misma fila
+//         localStorage.setItem("movieKey", JSON.stringify(movieKey))
+
+//         console.log(movieKey) //con esto cache que el problema es el local storage
+
+//         //actualizar en html 
+//         showDataHtml()
+
+//          //cambiar botones 
+//          const updateButton = document.querySelector("#boton-actualizar") 
+//          updateButton.style.display = "none" 
+ 
+//          const submitButton = document.querySelector("#boton-form")
+//          submitButton.style.display = "inline-block" 
+
+
+//        }
+
 
 
 //     }
-    
 
-// remove()
-// //console.log(JSON.parse(localStorage.getItem("movieKey", "movieKey[0]")))
+
+
 
